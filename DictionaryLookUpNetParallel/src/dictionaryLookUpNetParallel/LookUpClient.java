a2b18361330fe941ed3a0fffa88673b60d7e7fa2
Eric Bryan Klinginsmith <quinks@live.com>
2015-02-10 17:26:31 -0800

    Got things started on client side

--- DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpClient.java
+++ DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpClient.java
@@ -1,16 +1,68 @@
 package dictionaryLookUpNetParallel;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class LookUpClient {
 
+	private String ip = "";
+	private int port = 4444;
+	private Socket clientSocket = null;
+
 	public LookUpClient() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-	/**
-	 * @param args
-	 */
+	public LookUpClient(String ip, int port) {
+
+		super();
+		this.ip = ip;
+		this.port = port;
+		clientSocket = new Socket();
+		try {
+			clientSocket.connect(new InetSocketAddress(this.ip, this.port));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+
+			String ip = InetAddress.getLocalHost().getHostAddress();
+		    int port = 4444;
+			if (args.length > 0) {
+
+				if (args.length > 1) {
+
+					ip = args[0];
+					port = Integer.parseInt(args[1]);
+
+				}
+				else {
+
+					if (args[0].contains("."))
+						ip = args[0];
+					else
+						port = Integer.parseInt(args[0]);
+
+				}
+
+			}
+
+			LookUpClient luc = new LookUpClient(ip, port);
+			//luc.runClient(clientSocket, IPAddress, port);
+
+		}
+		catch (IOException e) {
+            e.printStackTrace();
+		}
 
 	}
 
