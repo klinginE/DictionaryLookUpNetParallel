ee24cf2ba13ad467426e0b6b6505775fd2fc3a2a
Eric Bryan Klinginsmith <quinks@live.com>
2015-02-10 16:49:15 -0800

    Added base outline for both server and client

---
+++ DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpClient.java
@@ -0,0 +1,17 @@
+package dictionaryLookUpNetParallel;
+
+public class LookUpClient {
+
+	public LookUpClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpServer.java
@@ -0,0 +1,17 @@
+package dictionaryLookUpNetParallel;
+
+public class LookUpServer {
+
+	public LookUpServer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
