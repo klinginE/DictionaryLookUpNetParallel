e4e60e39f3efca06a183e20f30c67d2dd22108c3
Eric Bryan Klinginsmith <quinks@live.com>
2015-02-11 15:26:42 -0800

    Made progress on initial compunication between client and server.

--- DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpClient.java
+++ DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpClient.java
@@ -1,16 +1,43 @@
 package dictionaryLookUpNetParallel;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.DatagramSocket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Paths;
 
 public class LookUpClient {
 
 	private String ip = "";
 	private int port = 4444;
 	private Socket clientSocket = null;
+	private String userName = "";
+
+	public static enum MSG_TYPE {
+
+		WELCOME(0),
+		CONFIRM(1),
+		NORMAL(2),
+		TERMINATE(3);
+
+		private final int value;
+		private MSG_TYPE(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+
+			return this.value;
+
+		}
+
+	}
 
 	public LookUpClient() {
 		super();
@@ -31,33 +58,229 @@ public class LookUpClient {
 
 	}
 
+	public String getWord(BufferedReader br) {
+
+		String word = "";
+        do {
+
+		    try {
+		        word = br.readLine();
+		    }
+		    catch (IOException ioe) {
+		        System.err.println("getWord() IO error trying to read word, because:" + ioe.getLocalizedMessage());
+		        System.exit(1);
+		    }
+		    if (word == null)
+		    	return word;
+		    word = word.trim();
+
+        } while (word.equals(""));
+
+	    word = word.toUpperCase();
+		return word;
+
+	}
+
+	public void runClient(File f) {
+
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(f));
+		}
+		catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		String word = "";
+		while ((word = getWord(br)) != null) {
+			//TODO:Send word to client NORMAL
+			//TODO:Print response if not normal NORMAL then quit
+		}
+		//TODO:Send ending message to client TERMINATE
+		try {
+			clientSocket.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void askForUserName() {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        userName = "";
+
+        do {
+
+	        System.out.print("Enter a Username: ");
+		    try {
+		    	userName = br.readLine();
+		        System.out.println();
+		    }
+		    catch (IOException ioe) {
+		        System.err.println("getUserName() IO error trying to read word, because:" + ioe.getLocalizedMessage());
+		        System.exit(1);
+		    }
+		    userName = userName.trim();
+
+        } while (userName.equals(""));
+
+        userName = userName.toUpperCase();
+
+	}
+
+	public boolean setUpConnection() {
+
+		try {
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			// Read welcome message
+			String line = input.readLine();
+			if (line == null) {
+
+				input.close();
+				return false;
+
+			}
+
+			line = line.trim();
+			if (!line.split(":")[0].equals(Integer.toString(MSG_TYPE.WELCOME.getValue()))) {
+
+				input.close();
+				return false;
+
+			}
+
+			PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+
+			int count = 0;
+			do {
+
+				askForUserName();
+				output.print(MSG_TYPE.CONFIRM.getValue() + ":" + userName + "\r\n");
+				output.flush();
+
+				line = input.readLine();
+				if (line == null) {
+
+					input.close();
+					output.close();
+					return false;
+
+				}
+				line = line.trim();
+				if (line.split(":")[0].equals(Integer.toString(MSG_TYPE.TERMINATE.getValue()))) {
+
+					input.close();
+					output.close();
+					return false;
+
+				}
+				else if (!line.split(":")[0].equals(Integer.toString(MSG_TYPE.CONFIRM.getValue())))
+					System.out.println("Username is already in use. Choose a different one");
+
+			} while ((++count) < 3 && line != null && !line.split(":")[0].equals(Integer.toString(MSG_TYPE.CONFIRM.getValue())));
+
+			input.close();
+			output.close();
+			if (line.split(":")[0].equals(Integer.toString(MSG_TYPE.CONFIRM.getValue())))
+				return true;
+
+		}
+		catch (IOException e) {
+			return false;
+		}
+		return false;
+
+	}
+
 	public static void main(String[] args) {
 		
 		try {
 
 			String ip = InetAddress.getLocalHost().getHostAddress();
 		    int port = 4444;
+		    File file = Paths.get(System.getProperty("user.dir"), "words.txt").toFile();
+		    String filePath = "";
+
 			if (args.length > 0) {
 
-				if (args.length > 1) {
+			    if (args.length > 1) {
 
-					ip = args[0];
-					port = Integer.parseInt(args[1]);
+			    	if (args[0].contains(".") && args[0].contains(":")) {
+
+			    		ip = args[0].split(":")[0];
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+			    		filePath = args[1];
+
+			    	}
+			    	else if (args[0].contains(".") && !args[0].contains(":")) {
+
+			    		ip = args[0];
+			    		filePath = args[1];
+
+			    	}
+			    	else if (!args[0].contains(".") && args[0].contains(":")) {
+
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+			    		filePath = args[1];
+
+			    	}
+			    	else {
+
+			    		ip = args[1].split(":")[0];
+			    		port = Integer.parseInt(args[1].split(":")[1]);
+			    		filePath = args[0];
+
+			    	}
 
 				}
 				else {
 
-					if (args[0].contains("."))
-						ip = args[0];
-					else
-						port = Integer.parseInt(args[0]);
+					if (args[0].contains(".") && args[0].contains(":")) {
+
+			    		ip = args[0].split(":")[0];
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+
+			    	}
+			    	else if (args[0].contains(".") && !args[0].contains(":")) {
+
+			    		ip = args[0];
+
+			    	}
+			    	else if (!args[0].contains(".") && args[0].contains(":")) {
+
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+
+			    	}
+			    	else {
+
+			    		filePath = args[0];
+
+			    	}
 
 				}
 
 			}
 
-			LookUpClient luc = new LookUpClient(ip, port);
-			//luc.runClient(clientSocket, IPAddress, port);
+			if (!filePath.equals(""))
+				file = new File(filePath);
+
+			if (file.exists() && !file.isDirectory() && file.isFile() && file.canRead()) {
+
+				LookUpClient luc = new LookUpClient(ip, port);
+				luc.setUpConnection();
+				luc.runClient(file);
+
+			}
+			else {
+
+				System.err.println("Error: " + file.getPath() + " does not exists, it is not a regular file, or it cannont be read.");
+				System.exit(1);
+
+			}
 
 		}
 		catch (IOException e) {
--- DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpServer.java
+++ DictionaryLookUpNetParallel/src/dictionaryLookUpNetParallel/LookUpServer.java
@@ -1,11 +1,364 @@
 package dictionaryLookUpNetParallel;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LookUpServer {
 
+	private static final int MAX_THREADS = (Runtime.getRuntime().availableProcessors() - 1);
+	private static final int MAX_CLIENTS = (MAX_THREADS * 10);
+
+	private volatile String ip = "";
+	private volatile int port = 4444;
+	private ServerSocket serverSocket = null;
+	private volatile boolean isRunning = false;
+	private HashMap<String, Socket> userSockets = null;
+	private ArrayList<ClientThread> threadPool = null;
+	private Queue<String> jobs = null;
+
+	public static enum MSG_TYPE {
+
+		WELCOME(0),
+		CONFIRM(1),
+		NORMAL(2),
+		TERMINATE(3);
+
+		private final int value;
+		private MSG_TYPE(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+
+			return this.value;
+
+		}
+
+	}
+
+	private class ClientThread extends Thread {
+
+		private volatile boolean isRunning = false;
+		private LookUpServer parrent = null;
+		private volatile String word = "";
+		private volatile String clientName = "";
+
+		public ClientThread() {
+			super();
+		}
+
+		public ClientThread(LookUpServer lus) {
+
+			super();
+			parrent = lus;
+
+		}
+
+		@Override
+		public void run() {
+			
+			isRunning = true;
+			while (isRunning && getParrentIsRunning()) {
+
+				while (clientName.equals("") && word.equals("") && isRunning && getParrentIsRunning()) {
+
+					try {
+						sleep(100l);
+					}
+					catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+				}
+				if (isRunning && getParrentIsRunning() && !word.equals("") && !clientName.equals("")) {
+
+					PrintWriter output = null;					
+					//TODO: process word and send results back to client
+					synchronized(this) {
+
+						this.word = "";
+						this.clientName = "";
+						//output.close();
+						output = null;
+
+					}
+
+				}
+
+			}
+			
+		}
+
+		public boolean getIsRunning() {
+			return isRunning;
+		}
+		public void setIsRunning(boolean isRun) {
+			isRunning = isRun;
+		}
+
+		public void processWord(String clientName, String clientWord) {
+			synchronized(this) {
+				if (this.clientName.equals("") && this.word.equals("")) {
+					this.clientName = clientName;
+					this.word = clientWord;
+				}
+			}
+		}
+		public boolean isProcessingWord() {
+			synchronized(this) {
+				return (!clientName.equals("") && !word.equals(""));
+			}
+		}
+
+		private boolean getParrentIsRunning() {
+			synchronized(parrent) {
+				return parrent.getIsRunning();
+			}
+		}
+
+	}
+
 	public LookUpServer() {
+		super();
+	}
+
+	public LookUpServer(String ip, int port) {
+
+		super();
+		this.ip = ip;
+		this.port = port;
+		try {
+			serverSocket = new ServerSocket();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			serverSocket.bind(new InetSocketAddress(this.ip, this.port));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		userSockets = new HashMap<String, Socket>(MAX_CLIENTS);
+		jobs = new LinkedList<String>();
+		threadPool = new ArrayList<ClientThread>(MAX_THREADS);
+		for (int i = 0; i < MAX_THREADS; i++) {
+			threadPool.add(new ClientThread(this));
+			threadPool.get(i).start();
+		}
+
+	}
+
+	public boolean getIsRunning() {
+		return isRunning;
+	}
+	public void setIsRunning(boolean isRun) {
+		isRunning = isRun;
+	}
+
+	public void runServer() {
+
+		isRunning = true;
+		while (isRunning) {
+
+			synchronized(userSockets) {
+				if (userSockets.size() >= MAX_CLIENTS) {
+					try {
+						Thread.sleep(100);
+					}
+					catch (InterruptedException e) {
+						continue;
+					}
+					continue;
+				}
+			}
+
+			Socket newClientSocket = null;
+			try {
+				newClientSocket = serverSocket.accept();
+			}
+			catch (IOException e) {
+				continue;
+			}
+			PrintWriter output = null;
+			BufferedReader input = null;
+			try {
+
+				output = new PrintWriter(newClientSocket.getOutputStream());
+			
+				output.print(MSG_TYPE.WELCOME.getValue() + ":" + "Welcome to the server, please send your username\r\n");
+				output.flush();
+
+				input = new BufferedReader(new InputStreamReader(newClientSocket.getInputStream()));
+
+				boolean validUsername = true;
+				int count = 0;
+				String line = "";
+				do {
+
+					line = input.readLine();
+					if (line == null) {
+	
+						input.close();
+						output.close();
+						break;
+	
+					}
+					line = line.trim();
+					if (!line.split(":")[0].equals(Integer.toString(MSG_TYPE.CONFIRM.getValue()))) {
+	
+						output.print(MSG_TYPE.TERMINATE.getValue() + ":" + "Invalid message\r\n");
+						output.flush();
+						output.close();
+						input.close();
+						break;
+	
+					}
+					line = line.split(":")[1];
+					synchronized(userSockets) {
+						for (String user : userSockets.keySet())
+							if (user.toUpperCase().equals(line.toUpperCase())) {
+	
+								validUsername = false;
+								output.print(MSG_TYPE.WELCOME.getValue() + ":" + "Invalid username\r\n");
+								output.flush();
+	
+							}
+					}
+
+				} while ((++count) < 3 && !validUsername);
+				if (count >= 3 || !validUsername) {
+
+					input.close();
+					output.close();
+					continue;
+
+				}
+
+				output.print(MSG_TYPE.CONFIRM.getValue() + ":" + "Username confirmed as " + line + "\r\n");
+				output.flush();
+
+				synchronized(userSockets) {
+
+					userSockets.put(line, newClientSocket);
+					jobs.add(line);
+
+				}
+				input.close();
+				output.close();
+
+			}
+			catch (IOException e) {
+				continue;
+			}
+
+		}
+
 	}
 
 	public static void main(String[] args) {
+
+		try {
+
+			String ip = InetAddress.getLocalHost().getHostAddress();
+		    int port = 4444;
+		    File file = Paths.get(System.getProperty("user.dir"), "dictionary.txt").toFile();
+		    String filePath = "";
+
+			if (args.length > 0) {
+
+			    if (args.length > 1) {
+
+			    	if (args[0].contains(".") && args[0].contains(":")) {
+
+			    		ip = args[0].split(":")[0];
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+			    		filePath = args[1];
+
+			    	}
+			    	else if (args[0].contains(".") && !args[0].contains(":")) {
+
+			    		ip = args[0];
+			    		filePath = args[1];
+
+			    	}
+			    	else if (!args[0].contains(".") && args[0].contains(":")) {
+
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+			    		filePath = args[1];
+
+			    	}
+			    	else {
+
+			    		ip = args[1].split(":")[0];
+			    		port = Integer.parseInt(args[1].split(":")[1]);
+			    		filePath = args[0];
+
+			    	}
+
+				}
+				else {
+
+					if (args[0].contains(".") && args[0].contains(":")) {
+
+			    		ip = args[0].split(":")[0];
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+
+			    	}
+			    	else if (args[0].contains(".") && !args[0].contains(":")) {
+
+			    		ip = args[0];
+
+			    	}
+			    	else if (!args[0].contains(".") && args[0].contains(":")) {
+
+			    		port = Integer.parseInt(args[0].split(":")[1]);
+
+			    	}
+			    	else {
+
+			    		filePath = args[0];
+
+			    	}
+
+				}
+
+			}
+
+			if (!filePath.equals(""))
+				file = new File(filePath);
+
+			if (file.exists() && !file.isDirectory() && file.isFile() && file.canRead()) {
+
+				LookUpServer luc = new LookUpServer(ip, port);
+				//TODO: start job handeler thread
+				luc.runServer();
+
+			}
+			else {
+
+				System.err.println("Error: " + file.getPath() + " does not exists, it is not a regular file, or it cannont be read.");
+				System.exit(1);
+
+			}
+
+		}
+		catch (IOException e) {
+            e.printStackTrace();
+		}
+
 	}
 
 }
